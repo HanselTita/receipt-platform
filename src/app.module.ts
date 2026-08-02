82470d7abc4b7c1f2f5595fffe2daa3275d9d7b3
Hansel <hanseltita@gmail.com>
2026-08-02 09:33:37 +0700

    feat: created user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [HealthModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HealthModule, PrismaModule, UsersModule],
 })
 export class AppModule {}
